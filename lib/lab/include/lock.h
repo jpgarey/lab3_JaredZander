fb744da61e754c51dd777aa816bcef89abf8a86e
Jared Garey <jared.garey@gmail.com>
2023-09-25 21:41:34 -0600

    Added work for deadlocking activity.

--- lib/lab/include/lock.h
+++ lib/lab/include/lock.h
@@ -11,6 +11,5 @@ int do_loop(struct k_timer *timer,
             char *src,
             k_timeout_t timeout);
 
-int unorphaned_lock(struct k_sem *semaphore, k_timeout_t timeout, int *counter);
-int orphaned_lock(struct k_sem *semaphore, k_timeout_t timeout, int *counter);
+//int orphaned_lock(struct k_sem *semaphore, k_timeout_t timeout, int *counter);
 void deadlock(struct k_sem *a, struct k_sem *b, int *counter);
--- test/test_lock/test_lock.c
+++ test/test_lock/test_lock.c
@@ -44,6 +44,71 @@ void test_loop_runs(void)
     TEST_ASSERT_EQUAL_INT(1, counter);
 }
 
+/*********************** Deadlock testing ************************/
+#define STACK_SIZE 500
+K_THREAD_STACK_DEFINE(left_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(right_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(sup_stack, STACK_SIZE);
+struct k_thread left_thread, right_thread, sup_thread;
+struct k_sem left, right;
+
+int counter0 = 2;
+int counter1 = 1;
+
+void deadlock_supervisor(void)
+{
+    k_sem_init(&left, 1, 1);
+    // Right starts locked,
+    k_sem_init(&right, 1, 1);
+    printf("- Creating threads\n");
+    k_tid_t l = k_thread_create(&left_thread, left_stack, STACK_SIZE,
+                                (k_thread_entry_t) deadlock,
+                                &left, &right, &counter0,
+                                K_PRIO_COOP(6),
+                                0,
+                                K_NO_WAIT);
+    k_tid_t r = k_thread_create(&right_thread, right_stack, STACK_SIZE,
+                                (k_thread_entry_t) deadlock,
+                                &right, &left, &counter1,
+                                K_PRIO_COOP(6),
+                                0,
+                                K_NO_WAIT);
+    printf("- Created threads\n");
+
+    int left_done = k_thread_join(l, K_MSEC(1000));
+    printf("- Waited left %d\n", left_done);
+    int right_done = k_thread_join(r, K_MSEC(1000));
+    printf("- Waited right %d\n", right_done);
+    TEST_ASSERT_EQUAL_INT(-EAGAIN, left_done);
+    TEST_ASSERT_EQUAL_INT(-EAGAIN, right_done);
+    printf("- Killing threads\n");
+    k_thread_abort(l);
+    k_thread_abort(r);
+    printf("- Killed threads\n");
+}
+
+void test_deadlock(void)
+{
+    printf("starting deadlock test\n");
+    k_thread_create(&sup_thread,
+                    sup_stack,
+                    STACK_SIZE,
+                    (k_thread_entry_t) deadlock_supervisor,
+                    NULL, NULL, NULL, // optional output p1, p2, and p3
+                    K_PRIO_COOP(7),
+                    0,
+                    K_NO_WAIT);
+    
+    k_thread_join(&sup_thread, K_FOREVER); // wait until the supervisor thread is done
+    printf("Finished supervisor\n");
+    
+    TEST_ASSERT_EQUAL_INT(0, k_sem_count_get(&left));
+    TEST_ASSERT_EQUAL_INT(0, k_sem_count_get(&right));
+    // Each counter should only be incremented twice
+    // Counters are initialized to values 2 and 1.
+    TEST_ASSERT_EQUAL_INT(4, counter0);
+    TEST_ASSERT_EQUAL_INT(3, counter1);
+}
 
 
 int main(void)
@@ -55,6 +120,7 @@ int main(void)
 
     RUN_TEST(test_loop_blocks);
     RUN_TEST(test_loop_runs);
+    RUN_TEST(test_deadlock);
 
     return UNITY_END();
 }
